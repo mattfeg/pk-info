0833a421f291eb00d9730cd0e647eb13c23f92bc
Matheus Ferreira <ferreira@MacBook-Air-de-Matheus.local>
2024-01-12 11:09:03 -0300

    Arquivo base.html criado e configurado nos templates

--- setup/wbpbase/tests.py
+++ setup/wbpbase/tests.py
@@ -1,32 +1,26 @@
 from django.test import TestCase
 
 # Create your tests here.
-def TestWBPBase(TestCase):
-    def setUp(self):
-        pass
-    
-    def tearDown(self):
-        pass
-    
-    def test_index_page(self):
+class TestWBPBase(TestCase):
+    def test_index_page_uses_template(self):
         response = self.client.get('')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'wbpbase/index.html')
+        self.assertTemplateUsed(response, 'wbpbase/index.html', 'Not using index.html')
     
-    def test_grid_page(self):
-        response = self.client.get('grid/')
+    def test_grid_page_uses_template(self):
+        response = self.client.get('/grid/')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'wbpbase/grid.html')
+        self.assertTemplateUsed(response, 'wbpbase/grid.html', 'Not using grid.html')
     
-    def test_contact_page(self):
-        response = self.client.get('contact/')
+    def test_contact_page_uses_template(self):
+        response = self.client.get('/contact/')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'wbpbase/contact.html')
+        self.assertTemplateUsed(response, 'wbpbase/contact.html', 'Not using contact.html')
     
-    def test_register_page(self):
-        response = self.client.get('register/')
+    def test_register_page_uses_template(self):
+        response = self.client.get('/register/')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'wbpbase/register.html')
+        self.assertTemplateUsed(response, 'wbpbase/register.html', 'Not using register.html')
 
 if __name__ == '__main__':
     TestWBPBase.main()
